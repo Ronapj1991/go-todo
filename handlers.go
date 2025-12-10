9529b635765647cc7024ceb611e2da4474eba551
Ronapj1991 <Ronapj1991@gmail.com>
2025-12-10 21:04:40 +0000

    add PATCH handler

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"strconv"
 	"strings"
@@ -34,5 +35,33 @@ func UpdateTodoHandler(store *TodoStore) http.HandlerFunc {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
+
+		id, err := parseIDFromPath(r.URL.Path)
+		if err != nil {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+
+		var updates map[string]interface{}
+		err = json.NewDecoder(r.Body).Decode(&updates)
+		if err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		err = store.UpdateTodoByID(id, updates)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		updatedTodo, ok := store.FindTodoByID(id)
+		if !ok {
+			http.Error(w, "Todo not found", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(updatedTodo)
 	}
 }
