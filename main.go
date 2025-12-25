7761852c51a3beb46f8204f981576badcc4ae42b
Ronapj1991 <Ronapj1991@gmail.com>
2025-12-25 14:58:06 +0000

    add everything to main.go

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	store := NewTodoStore()
+
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/todos", TodosHandler(store))
+	http.HandleFunc("/todos/", TodosByIDHandler(store))
 
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
